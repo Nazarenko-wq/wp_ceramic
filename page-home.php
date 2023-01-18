242c18a2d80df8d3e55ad9d4c611ff8c0e101ee0
Mihail Nazarenko <82104821+Nazarenko-wq@users.noreply.github.com>
2023-01-18 12:21:24 +0300

    feat: create page-home.php

---
+++ page-home.php
@@ -0,0 +1,138 @@
+<?php
+/*
+Template Name: Главная
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="banner">
+    <div class="banner_bg">
+        <div class="banner_window">
+            <div class="banner_content">
+                <div class="item_text">
+                    <h2 class="item_title">
+                        Ищете где купить механизмы Salvador?
+                    </h2>
+                    <p class="item_subtitle">
+                        Осуществляя ее заказ на общую сумму от 100 000 рублей, под все заказанные выключатели и розетки для открытой проводки в подарок вы получаете рамки из натурального бука нашего собственного производства.
+                    </p>
+                </div>
+                <div class="item_text">
+                    <h2 class="item_title">
+                        Ищете где купить механизмы Salvador?
+                    </h2>
+                    <p class="item_subtitle">
+                        Осуществляя ее заказ на общую сумму от 100 000 рублей, под все заказанные выключатели и розетки для открытой проводки в подарок вы получаете рамки из натурального бука нашего собственного производства.
+                    </p>
+                </div>
+                <div class="item_text">
+                    <h2 class="item_title">
+                        Ищете где купить механизмы Salvador?
+                    </h2>
+                    <p class="item_subtitle">
+                        Осуществляя ее заказ на общую сумму от 100 000 рублей, под все заказанные выключатели и розетки для открытой проводки в подарок вы получаете рамки из натурального бука нашего собственного производства.
+                    </p>
+                </div>
+                <div class="item_text">
+                    <h2 class="item_title">
+                        Ищете где купить механизмы Salvador?
+                    </h2>
+                    <p class="item_subtitle">
+                        Осуществляя ее заказ на общую сумму от 100 000 рублей, под все заказанные выключатели и розетки для открытой проводки в подарок вы получаете рамки из натурального бука нашего собственного производства.
+                    </p>
+                </div>
+            </div>
+            <div class="detail_block">
+                <button class="detail_btn">Подробнее</button>
+                <div class="btns_block">
+                    <div class="btn btn1 active"></div>
+                    <div class="btn btn2"></div>
+                    <div class="btn btn3"></div>
+                    <div class="btn btn4"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<main>
+    <div class="wrap_filters">
+        <div class="filters">
+            <div class="categories"></div>
+            <div class="brands"></div>
+        </div>
+    </div>
+    <div class="wrap_about">
+        <div class="short_discription_wrap">
+            <div class="short_discription">
+                <h2 class="short_title">Коротко о нас</h2>
+                <p class="short_subtitle">
+                    Мы, Фабрика Декора "КерамикЪ", уже много лет специализируемся на ретро электрике. За годы существования из увлекшихся любителей с горящими глазами наша команда превратилась в настоящих с большой буквы Профессионалов своего дела. Но искра в наших глазах совершенно не затухла! 
+                    Сегодня, мы планомерно работаем над тем, чтоб вырастить в таких же Профессионалов новых членов нашей команды.
+                </p>
+                <button class="short_btn">Подробнее</button>
+            </div>
+        </div>
+        <div class="service_wrap">
+            <div class="our_service">
+                <div class="our_service_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/service_item1.svg" alt="image">
+                    <h3 class="our_srvice_title">
+                        Наша специализация
+                    </h3>
+                    <p class="our_service_subtitle">
+                        Специализируемся на ретро электрике
+                    </p>
+                </div>
+                <div class="our_service_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/service_item2svg.svg" alt="image">
+                    <h3 class="our_srvice_title">
+                        Неповторимые товары
+                    </h3>
+                    <p class="our_service_subtitle">
+                        Мы производим уникальные рамки из натурального бука в разнообразных цветовых исполнениях.
+                    </p>
+                </div>
+                <div class="our_service_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/service_item3.svg" alt="image">
+                    <h3 class="our_srvice_title">
+                        Приятные цены
+                    </h3>
+                    <p class="our_service_subtitle">
+                        Мы предлагаем конкурентные цены на весь имеющийся товарный ассортимент, а также предоставляем скидки на множество позиций.
+                    </p>
+                </div>
+                <div class="our_service_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/service_item4.svg" alt="image">
+                    <h3 class="our_srvice_title">
+                        Услуги монтажа
+                    </h3>
+                    <p class="our_service_subtitle">
+                        Для вашего удобства, вместе с приобретаимыми товарами вы также можете заказать у нас их монтаж.
+                    </p>
+                </div>
+                <div class="our_service_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/service_item5.svg" alt="image">
+                    <h3 class="our_srvice_title">
+                        Возможность покраски
+                    </h3>
+                    <p class="our_service_subtitle">
+                        Не нашли нужного? - мы можем покрасить изделие в любой нужный цвет в соответствии с вашими нуждами и предпочтениями
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main> 
+<section class="comments_bg">
+    <div class="comments_wrap">
+        <div class="comments_content"></div>
+        <div class="comments_btns">
+            <div class="comment_btn comment_btn1 active_btn"></div>
+            <div class="comment_btn comment_btn2"></div>
+            <div class="comment_btn comment_btn3"></div>
+        </div>
+    </div>
+</section>    
+
+<?php get_footer(); ?>
